6c1e11be312c5952d69179bfdfcf98d8508e1aca
naomicerda1 <naomicerda1@gmail.com>
2020-03-20 18:48:54 -0700

    Initial Commit: Basic structure of Hello World!

---
+++ src/edu/whccd/ncerda/Main.java
@@ -0,0 +1,13 @@
+package edu.whccd.ncerda;
+
+/*
+    The purpose of this application is to display Hello World!
+    author:     Naomi Cerda
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World!");
+    }
+}
